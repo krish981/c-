4080b0b7c04492ba81262e56424d93295482874e
bansi43 <bansiundhad34@gmail.com>
2024-06-29 17:24:24 +0530

    29-06

--- practice16.cpp
+++ practice16.cpp
@@ -4,6 +4,11 @@ int main(){
     int i,j;
     int n=11;
     for(i=1;i<=5;i++){
-        for(j=1){}
+        for(j=1;j<=5;j++){
+            cout<<"\t"<<n;
+            n++;
+        }
+        cout<<endl;
+        n+=5;
     }
 }
