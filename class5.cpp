ce54a7c6fa8252483ccb1493fe838a48adcb6199
Janvi <sq.janvi@gmail.com>
2024-07-05 17:18:28 +0530

    5-07

--- class5.cpp
+++ class5.cpp
@@ -4,33 +4,32 @@ class Report{
     int adno;
     char name[50];
     float marks[5];
-    float avg=0;
-    float getavg(){
-        for(int i=1;i<=5;i++){
-            cout<<"Enter the marks of subject "<<i<<":";
-            cin>>marks[i-1];
-            avg+=marks[i-1];
-        }
-        avg/=5;
-        
-        return avg;
-    }
-
+    float avg;
+    float getavg();
+       
     public:
     void readinfo(){
         cout<<"Enter the adno :";
         cin>>adno;
         cout<<"Enter the name :";
         cin>>name;
-        getavg();
+         for(int i=1;i<=5;i++){
+            cout<<"Enter the marks of subject "<<i<<":";
+            cin>>marks[i-1];   
+        }
+        
 
     }
     void displayinfo(){
         cout<<"adno :"<<adno<<endl;
         cout<<"name :"<<name<<endl;
-        cout<<"Average of marks :"<<getavg();
+        cout<<"Average of marks :"<<getavg()<<endl;
     }
 };
+float Report::getavg(){
+    avg=(marks[0]+marks[1]+marks[2]+marks[3]+marks[4])/5;
+    return avg;
+}
 int main(){
     class Report stu;
     stu.readinfo();
